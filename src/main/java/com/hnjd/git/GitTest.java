5f4f63fcd67cc0d3506b9f6a25403a078253111e
zhangsan <889900@qq.com>
2022-10-25 11:37:40 +0800

    second commit

--- src/main/java/com/hnjd/git/GitTest.java
+++ src/main/java/com/hnjd/git/GitTest.java
@@ -2,6 +2,7 @@ package com.hnjd.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello,Git!!!");
+        System.out.println("Hello,Git1!!!");
+        System.out.println("Hello,Git2!!!");
     }
 }
